405f5f5a3478e15a7a0516f21791d95f44b5344b
eclipser_user <792579876@qq.com>
2020-05-02 14:05:39 +0800

    MY First commit in myeclipse

---
+++ src/com/itzh/test/Git.java
@@ -0,0 +1,13 @@
+package com.itzh.test;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("你好");
+ 
+	}
+
+}
